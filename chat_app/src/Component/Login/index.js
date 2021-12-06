744f57db7bc62c224cd59acb2680e39d2f123ed3
duong dh <duongdh@ames.edu.vn>
2021-12-06 17:37:20 +0700

    ádasd

--- chat_app/src/Component/Login/index.js
+++ chat_app/src/Component/Login/index.js
@@ -1,65 +1,106 @@
-import React from 'react'
-import { Form, Input, Button, Checkbox,Row, Col,Typography  } from 'antd';
-import { TeamOutlined ,ShoppingOutlined} from '@ant-design/icons';
-import firebase,{ auth } from '../../Firebase/config'
+import React, { useEffect, useState } from "react";
+import { Form, Input, Button, Row, Col, Typography, Avatar, Image } from "antd";
+import {
+  TeamOutlined,
+  ShoppingOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import firebase, { auth } from "../../Firebase/config";
 const Google_provider = new firebase.auth.GoogleAuthProvider();
 
 function Login() {
-    const onFinish = (values) => {
-        console.log('Success:', values);
-      };
-    const hanldeGoogle = () =>{
-        auth.signInWithPopup(Google_provider)
-    }
-    auth.onAuthStateChanged( (user) => {
-    console.log("🙉🍀 --> user", {user})
-        
-    })
-    return (
-        <Row justify="center" style={{height:'100vh'}}>
-            <Col span={8} style={{border:'1px solid ',textAlign:'center',padding:10}} > 
+  const onFinish = (values) => {};
+  //-----------------------------------------------
+  const [usersLogin, SetUsersLogin] = useState();
+  console.log("🙉🍀 __ usersLogin", usersLogin);
 
-                <Typography.Title level={2} > 
-                    Login Em
-                </Typography.Title>
-                <Form  name="basic"   onFinish={onFinish} 
-                >
-                    <Form.Item  name="username"  style={{    marginBottom:5}}
-                    rules={[{  required: true,  message: 'Please input your username!', },]}
-                    >
-                    <Input placeholder="Username" prefix={<TeamOutlined />}/>
-                    </Form.Item>
-            
-                    <Form.Item  name="password" style={{    marginBottom:5}}
-                    rules={[ { required: true, message: 'Please input your password!',  },  ]}
-                    >
-                    <Input.Password placeholder="Username" prefix={<ShoppingOutlined />}/>
-                    </Form.Item>
-                    
-                    <Form.Item style={{marginBottom:10}}>
-                        <Button type="primary" htmlType="submit" style={{width:'100%'}}>
-                            Đăng Nhập
-                        </Button>
-                    </Form.Item>
-                </Form>
-                <span style={{display:'flex',justifyContent:'end',marginBottom:10}}>
-                    <a style={{marginRight:10}}>Đăng ký nhanh</a>
-                    <a>Quên mật khẩu ?</a>
-                </span>
+  const hanldeGoogle = () => {
+    auth.signInWithPopup(Google_provider);
+  };
+  const hanldeGoogle_signOut = () => {
+    auth.signOut(Google_provider);
+  };
+  auth.onAuthStateChanged((user) => {
+    SetUsersLogin(user?._delegate);
+  });
+  //-----------------------------------------------
 
-                <span style={{display:'flex'}}>
-                    <Button style={{width:'50%'}} onClick={hanldeGoogle}>
-                        Đăng Nhập bằng Google
-                    </Button>
-                    <Button style={{width:'50%'}} >
-                        Đăng Nhập bằng FaceBook
-                    </Button>
-                </span>
-                
-            </Col>
-      </Row>
-        
-    )
+  return (
+    <Row justify="center" style={{ height: "100vh" }}>
+      <Col
+        span={10}
+        style={{ border: "1px solid ", textAlign: "center", padding: 10 }}
+      >
+        <Typography.Title level={2}>Login Em</Typography.Title>
+        <Form name="basic" onFinish={onFinish}>
+          <Form.Item
+            name="username"
+            style={{ marginBottom: 5 }}
+            rules={[{ required: true, message: "Please input your username!" }]}
+          >
+            <Input placeholder="Username" prefix={<TeamOutlined />} />
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            style={{ marginBottom: 5 }}
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password
+              placeholder="Username"
+              prefix={<ShoppingOutlined />}
+            />
+          </Form.Item>
+
+          <Form.Item style={{ marginBottom: 10 }}>
+            <Button type="primary" htmlType="submit" style={{ width: "100%" }}>
+              Đăng Nhập
+            </Button>
+          </Form.Item>
+        </Form>
+        <span
+          style={{ display: "flex", justifyContent: "end", marginBottom: 10 }}
+        >
+          <span style={{ marginRight: 10 }}>Đăng ký nhanh</span>
+          <span>Quên mật khẩu ?</span>
+        </span>
+
+        <span style={{ display: "flex" }}>
+          <Button style={{ width: "50%" }} onClick={hanldeGoogle}>
+            Đăng Nhập Google
+          </Button>
+          <Button style={{ width: "50%" }}>Đăng Nhập FaceBook</Button>
+        </span>
+      </Col>
+      <div
+        style={{
+          position: "absolute",
+          right: 50,
+          top: 10,
+        }}
+      >
+        {usersLogin && (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "end",
+              flexDirection: "column",
+            }}
+          >
+            <div>
+              {" "}
+              Chào bạn{" "}
+              <span style={{ color: "#f56a00" }}>
+                {usersLogin.displayName}
+              </span>{" "}
+              <Avatar size={40} src={usersLogin.photoURL} />
+            </div>
+            <Button onClick={hanldeGoogle_signOut}>Đăng xuất</Button>
+          </div>
+        )}
+      </div>
+    </Row>
+  );
 }
 
-export default Login
+export default Login;
--- chat_app/src/Firebase/config.js
+++ chat_app/src/Firebase/config.js
@@ -1,22 +1,22 @@
 import firebase from "firebase/compat";
-import 'firebase/auth';
-import 'firebase/analytics';
-import 'firebase/firestore';
+import "firebase/auth";
+import "firebase/analytics";
+import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDaOVsJ1CalPDNgAMi19K9JpXBYuQxJtJs",
-    authDomain: "chatapp-a1aff.firebaseapp.com",
-    projectId: "chatapp-a1aff",
-    storageBucket: "chatapp-a1aff.appspot.com",
-    messagingSenderId: "243842531366",
-    appId: "1:243842531366:web:2bd913727d7322659f66e7",
-    measurementId: "G-WVJJB4NPZM"
-  };
+  apiKey: "AIzaSyDaOVsJ1CalPDNgAMi19K9JpXBYuQxJtJs",
+  authDomain: "chatapp-a1aff.firebaseapp.com",
+  projectId: "chatapp-a1aff",
+  storageBucket: "chatapp-a1aff.appspot.com",
+  messagingSenderId: "243842531366",
+  appId: "1:243842531366:web:2bd913727d7322659f66e7",
+  measurementId: "G-WVJJB4NPZM",
+};
 
 firebase.initializeApp(firebaseConfig);
-firebase.analytics()
+firebase.analytics();
 
 const auth = firebase.auth();
-const db = firebase.firestore()
-export {db,auth}
-export default firebase
+const db = firebase.firestore();
+export { db, auth };
+export default firebase;
