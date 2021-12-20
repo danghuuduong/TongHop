7fab19b3024636eab76326aeb1cf476a21902a13
dang huu duong <danghuuduong98@gmail.com>
2021-12-20 21:47:26 +0700

    css Flash Sale

--- chat_app/src/Component/Bodys/Body.js
+++ chat_app/src/Component/Bodys/Body.js
@@ -12,9 +12,62 @@ function Body() {
   return (
     <div className="bgr_gray">
       <Container style={{ marginTop: 0, paddingTop: 10 }}>
+        <Row className="flash">
+          <div style={{display:'flex',justifyContent:'space-around',margin:'15px 0px'}}>
+            <div style={{textAlign:'center'}}>
+              <img src="https://cdn0.fahasa.com/media/wysiwyg/icon-menu/ico_flashsale.png" 
+              style={{width:50}}
+              alt alt='...' />
+              <div>Flash Sale</div>
+            </div>
+
+            <div style={{textAlign:'center'}}>
+              <img src="https://cdn0.fahasa.com/media/wysiwyg/icon-menu/ico_mgg.png" 
+              style={{width:50}}
+              alt alt='...' />
+              <div>Mã Giảm Giá</div>
+            </div>
+
+            <div style={{textAlign:'center'}}>
+              <img src="https://cdn0.fahasa.com/media/wysiwyg/icon-menu/ico_PCSC.png" 
+              style={{width:50}}
+              alt alt='...' />
+              <div>Phiên Chợ Sách Cũ</div>
+            </div>
+
+            <div style={{textAlign:'center'}}>
+              <img src="https://cdn0.fahasa.com/media/wysiwyg/icon-menu/ico-xuhuong.png" 
+              style={{width:50}}
+              alt alt='...' />
+              <div>Xu Hướng</div>
+            </div>
+
+            <div style={{textAlign:'center'}}>
+              <img src="https://cdn0.fahasa.com/media/wysiwyg/icon-menu/ico_covid-2.png" 
+              style={{width:50}}
+              alt alt='...' />
+              <div>Health Care</div>
+            </div>
+
+            <div style={{textAlign:'center'}}>
+              <img src="https://cdn0.fahasa.com/media/wysiwyg/icon-menu/ico_vpp_hot.png" 
+              style={{width:50}}
+              alt alt='...' />
+              <div>Văn Phòng Phẩm </div>
+            </div>
+
+            <div style={{textAlign:'center'}}>
+              <img src="https://cdn0.fahasa.com/media/wysiwyg/icon-menu/ico_goiy.png" 
+              style={{width:50}}
+              alt alt='...' />
+              <div>Gợi Ý Cho Bạn</div>
+            </div>
+
+          </div>
+        </Row>
         <Row>  
           <>
-            <Col  style={{ padding: 0, margin: "0 10px",   backgroundColor: "#fff",   }}  >
+            <Col  style={{ padding: 0, margin: "0 10px 0 0",   backgroundColor: "#fff",   }}  >
               <NavLeft />
             </Col>
             <Col xs={10}   style={{ backgroundColor: "#fff", padding: 0,margin:0 }} > 
